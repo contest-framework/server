4b4b60d130186fbd8da9bf82d4ac14742857285c
Kevin Goslar <kevin.goslar@gmail.com>
2021-10-04 17:33:25 -0500

    dprint.json tertestrial-command.ts

--- text-run/tertestrial-command.ts
+++ text-run/tertestrial-command.ts
@@ -1,35 +1,35 @@
-import * as childProcess from "child_process";
-import * as os from "os";
-import * as path from "path";
-import * as tr from "text-runner";
-import * as util from "util";
-const asyncExec = util.promisify(childProcess.exec);
+import * as childProcess from "child_process"
+import * as os from "os"
+import * as path from "path"
+import * as tr from "text-runner"
+import * as util from "util"
+const asyncExec = util.promisify(childProcess.exec)
 
 export async function tertestrialCommand(action: tr.actions.Args) {
-  const documented = action.region.text().trim().replace(/^tertestrial /, "");
-  action.name(`Valid Tertestrial command: ${documented}`);
-  const existing = await getExistingCommands();
+  const documented = action.region.text().trim().replace(/^tertestrial /, "")
+  action.name(`Valid Tertestrial command: ${documented}`)
+  const existing = await getExistingCommands()
   if (!existing.includes(documented)) {
-    throw new Error(`Tertestrial has no command "${documented}"\n\Known commands: ${existing.join(" | ")}`);
+    throw new Error(`Tertestrial has no command "${documented}"\n\Known commands: ${existing.join(" | ")}`)
   }
 }
 
 async function getExistingCommands(): Promise<string[]> {
-  const { stdout, stderr } = await asyncExec(path.join(__dirname, "..", "target", "debug", "tertestrial") + " help");
-  const output = stdout.trim() + stderr.trim();
-  let inSubcommandsSection = false;
-  const result = [];
-  const firstWordRE = /^\s*(\w+)/; // extracts the first word in the given string
-  const lines = output.split(os.EOL);
+  const { stdout, stderr } = await asyncExec(path.join(__dirname, "..", "target", "debug", "tertestrial") + " help")
+  const output = stdout.trim() + stderr.trim()
+  let inSubcommandsSection = false
+  const result = []
+  const firstWordRE = /^\s*(\w+)/ // extracts the first word in the given string
+  const lines = output.split(os.EOL)
   for (const line of lines) {
     if (line.startsWith("SUBCOMMANDS:")) {
-      inSubcommandsSection = true;
-      continue;
+      inSubcommandsSection = true
+      continue
     }
     if (inSubcommandsSection) {
-      const matches = line.match(firstWordRE);
-      result.push(matches[1]);
+      const matches = line.match(firstWordRE)
+      result.push(matches[1])
     }
   }
-  return result;
+  return result
 }
