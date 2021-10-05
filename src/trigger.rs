2132db29b409acfd58de57e9dc6c040e4102bfb4
Kevin Goslar <kevin.goslar@gmail.com>
2021-10-04 19:04:30 -0500

    Organize tests

--- src/trigger.rs
+++ src/trigger.rs
@@ -70,176 +70,190 @@ pub fn from_string(line: &str) -> Result<Trigger, UserError> {
 
 #[cfg(test)]
 mod tests {
-    use super::*;
 
-    #[test]
-    fn from_line_test_all() {
-        let have = from_string(r#"{ "command": "testAll" }"#).unwrap();
-        let want = Trigger {
-            command: "testAll".to_string(),
-            file: None,
-            line: None,
-        };
-        assert_eq!(have, want);
-    }
+    mod from_string {
 
-    #[test]
-    fn from_line_filename() {
-        let have = from_string(r#"{ "command": "testFile", "file": "foo.rs" }"#).unwrap();
-        let want = Trigger {
-            command: "testFile".to_string(),
-            file: Some("foo.rs".to_string()),
-            line: None,
-        };
-        assert_eq!(have, want);
-    }
+        use super::super::*;
 
-    #[test]
-    fn from_line_filename_line() {
-        let have =
-            from_string(r#"{ "command": "testFunction", "file": "foo.rs", "line": 12 }"#).unwrap();
-        let want = Trigger {
-            command: "testFunction".to_string(),
-            file: Some("foo.rs".to_string()),
-            line: Some(12),
-        };
-        assert_eq!(have, want);
-    }
+        #[test]
+        fn test_all() {
+            let give = r#"{ "command": "testAll" }"#;
+            let have = from_string(give).unwrap();
+            let want = Trigger {
+                command: "testAll".to_string(),
+                file: None,
+                line: None,
+            };
+            assert_eq!(have, want);
+        }
 
-    #[test]
-    fn from_line_filename_extra_fields() {
-        let have =
-            from_string(r#"{ "command": "testFile", "file": "foo.rs", "other": "12" }"#).unwrap();
-        let want = Trigger {
-            command: "testFile".to_string(),
-            file: Some(String::from("foo.rs")),
-            line: None,
-        };
-        assert_eq!(have, want);
-    }
+        #[test]
+        fn filename() {
+            let give = r#"{ "command": "testFile", "file": "foo.rs" }"#;
+            let have = from_string(give).unwrap();
+            let want = Trigger {
+                command: "testFile".to_string(),
+                file: Some("foo.rs".to_string()),
+                line: None,
+            };
+            assert_eq!(have, want);
+        }
 
-    #[test]
-    fn from_string_invalid_json() {
-        let line = "{\"filename}";
-        let have = from_string(line);
-        let want = UserError::InvalidTrigger {
-            line: line.to_string(),
-            err: "EOF while parsing a string at line 1 column 11".to_string(),
-        };
-        match have {
-            Ok(_) => panic!("this shouldn't work"),
-            Err(err) => assert_eq!(err, want),
+        #[test]
+        fn filename_line() {
+            let give = r#"{ "command": "testFunction", "file": "foo.rs", "line": 12 }"#;
+            let have = from_string(give).unwrap();
+            let want = Trigger {
+                command: "testFunction".to_string(),
+                file: Some("foo.rs".to_string()),
+                line: Some(12),
+            };
+            assert_eq!(have, want);
+        }
+
+        #[test]
+        fn filename_extra_fields() {
+            let give = r#"{ "command": "testFile", "file": "foo.rs", "other": "12" }"#;
+            let have = from_string(give).unwrap();
+            let want = Trigger {
+                command: "testFile".to_string(),
+                file: Some(String::from("foo.rs")),
+                line: None,
+            };
+            assert_eq!(have, want);
+        }
+
+        #[test]
+        fn from_string_invalid_json() {
+            let give = "{\"filename}";
+            let have = from_string(give);
+            let want = UserError::InvalidTrigger {
+                line: give.to_string(),
+                err: "EOF while parsing a string at line 1 column 11".to_string(),
+            };
+            match have {
+                Ok(_) => panic!("this shouldn't work"),
+                Err(err) => assert_eq!(err, want),
+            }
         }
     }
 
-    #[test]
-    fn eq_match() {
-        let trigger1 = Trigger {
-            command: "testFunction".to_string(),
-            file: Some("filename".to_string()),
-            line: Some(12),
-        };
-        let trigger2 = Trigger {
-            command: "testFunction".to_string(),
-            file: Some("filename".to_string()),
-            line: Some(12),
-        };
-        assert!(trigger1 == trigger2);
+    mod eq {
+        use super::super::*;
+
+        #[test]
+        fn matching() {
+            let trigger1 = Trigger {
+                command: "testFunction".to_string(),
+                file: Some("filename".to_string()),
+                line: Some(12),
+            };
+            let trigger2 = Trigger {
+                command: "testFunction".to_string(),
+                file: Some("filename".to_string()),
+                line: Some(12),
+            };
+            assert!(trigger1 == trigger2);
+        }
+
+        #[test]
+        fn mismatching_filename() {
+            let trigger1 = Trigger {
+                command: "testFunction".to_string(),
+                file: Some("filename1".to_string()),
+                line: Some(12),
+            };
+            let trigger2 = Trigger {
+                command: "testFunction".to_string(),
+                file: Some("filename2".to_string()),
+                line: Some(12),
+            };
+            assert!(trigger1 != trigger2);
+        }
+
+        #[test]
+        fn mismatching_line() {
+            let trigger1 = Trigger {
+                command: "testFunction".to_string(),
+                file: Some("filename".to_string()),
+                line: Some(12),
+            };
+            let trigger2 = Trigger {
+                command: "testFunction".to_string(),
+                file: Some("filename".to_string()),
+                line: Some(11),
+            };
+            assert!(trigger1 != trigger2);
+        }
     }
 
-    #[test]
-    fn eq_mismatching_filename() {
-        let trigger1 = Trigger {
-            command: "testFunction".to_string(),
-            file: Some("filename1".to_string()),
-            line: Some(12),
-        };
-        let trigger2 = Trigger {
-            command: "testFunction".to_string(),
-            file: Some("filename2".to_string()),
-            line: Some(12),
-        };
-        assert!(trigger1 != trigger2);
-    }
+    mod matches_client_trigger {
+        use super::super::*;
 
-    #[test]
-    fn eq_mismatching_line() {
-        let trigger1 = Trigger {
-            command: "testFunction".to_string(),
-            file: Some("filename".to_string()),
-            line: Some(12),
-        };
-        let trigger2 = Trigger {
-            command: "testFunction".to_string(),
-            file: Some("filename".to_string()),
-            line: Some(11),
-        };
-        assert!(trigger1 != trigger2);
-    }
+        #[test]
+        fn matching() {
+            let config = Trigger {
+                command: "testFunction".to_string(),
+                file: Some("**/*.rs".to_string()),
+                line: Some(12),
+            };
+            let give = Trigger {
+                command: "testFunction".to_string(),
+                file: Some("foo.rs".to_string()),
+                line: Some(12),
+            };
+            assert!(config.matches_client_trigger(&give).unwrap());
+            let give = Trigger {
+                command: "testFunction".to_string(),
+                file: Some("foo/bar.rs".to_string()),
+                line: Some(12),
+            };
+            assert!(config.matches_client_trigger(&give).unwrap());
+        }
 
-    #[test]
-    fn matches_match() {
-        let config = Trigger {
-            command: "testFunction".to_string(),
-            file: Some("**/*.rs".to_string()),
-            line: Some(12),
-        };
-        let give = Trigger {
-            command: "testFunction".to_string(),
-            file: Some("foo.rs".to_string()),
-            line: Some(12),
-        };
-        assert!(config.matches_client_trigger(&give).unwrap());
-        let give = Trigger {
-            command: "testFunction".to_string(),
-            file: Some("foo/bar.rs".to_string()),
-            line: Some(12),
-        };
-        assert!(config.matches_client_trigger(&give).unwrap());
-    }
+        #[test]
+        fn mismatching_command() {
+            let config = Trigger {
+                command: "testFunction".to_string(),
+                file: Some("filename".to_string()),
+                line: Some(12),
+            };
+            let give = Trigger {
+                command: "testFile".to_string(),
+                file: Some("filename".to_string()),
+                line: Some(12),
+            };
+            assert!(!config.matches_client_trigger(&give).unwrap());
+        }
 
-    #[test]
-    fn matches_mismatching_command() {
-        let config = Trigger {
-            command: "testFunction".to_string(),
-            file: Some("filename".to_string()),
-            line: Some(12),
-        };
-        let give = Trigger {
-            command: "testFile".to_string(),
-            file: Some("filename".to_string()),
-            line: Some(12),
-        };
-        assert!(!config.matches_client_trigger(&give).unwrap());
-    }
+        #[test]
+        fn mismatching_file() {
+            let config = Trigger {
+                command: "testFunction".to_string(),
+                file: Some("filename".to_string()),
+                line: Some(12),
+            };
+            let give = Trigger {
+                command: "testFile".to_string(),
+                file: Some("filename2".to_string()),
+                line: Some(12),
+            };
+            assert!(!config.matches_client_trigger(&give).unwrap());
+        }
 
-    #[test]
-    fn matches_mismatching_file() {
-        let config = Trigger {
-            command: "testFunction".to_string(),
-            file: Some("filename".to_string()),
-            line: Some(12),
-        };
-        let give = Trigger {
-            command: "testFile".to_string(),
-            file: Some("filename2".to_string()),
-            line: Some(12),
-        };
-        assert!(!config.matches_client_trigger(&give).unwrap());
-    }
-
-    #[test]
-    fn matches_mismatching_line() {
-        let config = Trigger {
-            command: "testFunction".to_string(),
-            file: Some("filename".to_string()),
-            line: Some(12),
-        };
-        let give = Trigger {
-            command: "testFile".to_string(),
-            file: Some("filename".to_string()),
-            line: Some(11),
-        };
-        assert!(!config.matches_client_trigger(&give).unwrap());
+        #[test]
+        fn mismatching_line() {
+            let config = Trigger {
+                command: "testFunction".to_string(),
+                file: Some("filename".to_string()),
+                line: Some(12),
+            };
+            let give = Trigger {
+                command: "testFile".to_string(),
+                file: Some("filename".to_string()),
+                line: Some(11),
+            };
+            assert!(!config.matches_client_trigger(&give).unwrap());
+        }
     }
 }
