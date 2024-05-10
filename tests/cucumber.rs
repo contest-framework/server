cc3b0f4fe6da3f41ea23ad1b5121a7ca92e4528b
Kevin Goslar <kevin.goslar@gmail.com>
2024-05-10 11:26:17 -0500

    cucumber.rs

--- tests/cucumber.rs
+++ tests/cucumber.rs
@@ -4,49 +4,53 @@ use cucumber::{given, then, when, World};
 // Accepts both sync/async and fallible/infallible functions.
 #[world(init = Self::new)]
 pub struct AnimalWorld {
-    cat: Cat,
+  cat: Cat,
 }
 
 impl AnimalWorld {
-    fn new() -> Self {
-        Self {
-            cat: Cat { hungry: true },
-        }
+  fn new() -> Self {
+    Self {
+      cat: Cat { hungry: true },
     }
+  }
 }
 
 // These `Cat` definitions would normally be inside your project's code,
 // not test code, but we create them here for the show case.
 #[derive(Debug, Default)]
 struct Cat {
-    pub hungry: bool,
+  pub hungry: bool,
 }
 
 impl Cat {
-    fn feed(&mut self) {
-        self.hungry = false;
-    }
+  fn feed(&mut self) {
+    self.hungry = false;
+  }
 }
 
 // Steps are defined with `given`, `when` and `then` attributes.
 #[given("a hungry cat")]
 fn hungry_cat(world: &mut AnimalWorld) {
-    world.cat.hungry = true;
+  world.cat.hungry = true;
 }
 
 // Don't forget to additionally `use cucumber::when;`.
 
 #[when("I feed the cat")]
 fn feed_cat(world: &mut AnimalWorld) {
-    world.cat.feed();
+  world.cat.feed();
 }
 
 #[then("the cat is not hungry")]
 fn cat_is_fed(world: &mut AnimalWorld) {
-    assert!(!world.cat.hungry);
+  assert!(!world.cat.hungry);
 }
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() {
-    AnimalWorld::run("features").await;
+  AnimalWorld::cucumber()
+    .fail_fast()
+    .max_concurrent_scenarios(Some(2))
+    .run_and_exit("features/")
+    .await;
 }
