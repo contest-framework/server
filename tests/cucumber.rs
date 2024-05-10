818d5425d916733748aad75a19e1040806d778f7
Kevin Goslar <kevin.goslar@gmail.com>
2024-05-10 14:26:41 -0500

    cucumber.rs

--- tests/cucumber.rs
+++ tests/cucumber.rs
@@ -1,6 +1,7 @@
+use std::process::Stdio;
+
 use cucumber::gherkin::Step;
 use cucumber::{given, then, when, World};
-use std::process::Stdio;
 use tempfile::TempDir;
 use tokio::io::{AsyncBufReadExt, BufReader, BufWriter};
 use tokio::process::{Child, ChildStdin, ChildStdout, Command};
@@ -8,19 +9,17 @@ use tokio::process::{Child, ChildStdin, ChildStdout, Command};
 #[derive(Debug, World)]
 // Accepts both sync/async and fallible/infallible functions.
 #[world(init = Self::new)]
-pub struct AnimalWorld {
-  cat: Cat,
+pub struct CukeWorld {
   workspace: TempDir,
   cmd: Option<Child>,
   stdin: Option<BufWriter<ChildStdin>>,
   stdout: Option<BufReader<ChildStdout>>,
 }
 
-impl AnimalWorld {
+impl CukeWorld {
   fn new() -> Self {
     let root = tempfile::tempdir().unwrap();
     Self {
-      cat: Cat { hungry: true },
       workspace: root,
       cmd: None,
       stdin: None,
@@ -30,7 +29,7 @@ impl AnimalWorld {
 }
 
 #[given("Tertestrial is running")]
-async fn tertestrial_running(world: &mut AnimalWorld) {
+async fn tertestrial_running(world: &mut CukeWorld) {
   let cwd = std::env::current_dir().unwrap();
   let tertestrial_path = cwd.join("target").join("debug").join("tertestrial");
   let mut cmd = Command::new(tertestrial_path)
@@ -50,7 +49,7 @@ async fn tertestrial_running(world: &mut AnimalWorld) {
 }
 
 #[then("it prints:")]
-async fn it_prints(world: &mut AnimalWorld, step: &Step) {
+async fn it_prints(world: &mut CukeWorld, step: &Step) {
   let want = step.docstring.as_ref().unwrap().trim();
   let reader = world.stdout.as_mut().unwrap();
   let mut output = String::new();
@@ -62,37 +61,9 @@ async fn it_prints(world: &mut AnimalWorld, step: &Step) {
   assert_eq!(&have, want);
 }
 
-// -------------------------------------------------------------------------------------------------------------------
-
-#[derive(Debug, Default)]
-struct Cat {
-  pub hungry: bool,
-}
-
-impl Cat {
-  fn feed(&mut self) {
-    self.hungry = false;
-  }
-}
-
-#[given("a hungry cat")]
-fn hungry_cat(world: &mut AnimalWorld) {
-  world.cat.hungry = true;
-}
-
-#[when("I feed the cat")]
-fn feed_cat(world: &mut AnimalWorld) {
-  world.cat.feed();
-}
-
-#[then("the cat is not hungry")]
-fn cat_is_fed(world: &mut AnimalWorld) {
-  assert!(!world.cat.hungry);
-}
-
 #[tokio::main(flavor = "current_thread")]
 async fn main() {
-  AnimalWorld::cucumber()
+  CukeWorld::cucumber()
     .fail_fast()
     .max_concurrent_scenarios(Some(1))
     .run_and_exit("features/")
