b257d8eae62428e38adfbc16f4e1b81c5adb5368
Kevin Goslar <kevin.goslar@gmail.com>
2024-05-13 13:40:41 -0500

    main.rs

--- tests/cucumber/main.rs
+++ tests/cucumber/main.rs
@@ -18,7 +18,7 @@ async fn file_with_content(world: &mut TertestrialWorld, step: &Step, filename: 
 async fn start_tertestrial(world: &mut TertestrialWorld, command: String) {
   let words = shellwords::split(&command).unwrap();
   let ([cmd], args) = words.split_at(1) else {
-    panic!("cannot split words");
+    panic!("this error never happens");
   };
   if cmd != "tertestrial" {
     panic!("can only execute tertestrial");
