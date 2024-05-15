27aa0b3726f86d07172a0c908db71fbb3703db1a
Kevin Goslar <kevin.goslar@gmail.com>
2024-05-15 16:32:18 -0500

    file_action.rs mod.rs

--- src/config/file/data/mod.rs
+++ src/config/file/data/mod.rs
@@ -2,14 +2,14 @@
 //! This is for de-serializing the config file into Rust data structures.
 //! We then convert the data structure to validated config data in Rust.
 
-mod action;
+mod file_action;
 mod file_after_run;
 mod file_before_run;
 mod file_configuration;
 mod file_options;
 mod file_var;
 
-pub use action::FileAction;
+pub use file_action::FileAction;
 pub use file_after_run::FileAfterRun;
 pub use file_before_run::FileBeforeRun;
 pub use file_configuration::FileConfiguration;
