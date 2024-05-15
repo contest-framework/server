20d5ca6f7497ec77b781db700ef5626532cccab0
Kevin Goslar <kevin.goslar@gmail.com>
2024-05-15 15:00:07 -0500

    pattern.rs

--- src/config/data/pattern.rs
+++ src/config/data/pattern.rs
@@ -10,17 +10,17 @@ pub enum Pattern {
 
 impl Pattern {
   pub fn matches_trigger(&self, trigger: &Trigger) -> bool {
-    if self == &Pattern::TestAll && trigger == &Trigger::TestAll {
-      return true;
-    }
-    if let Pattern::TestFile { files } = &self {
-      if let Trigger::TestFile { file } = &trigger {
-        return files.matches(file);
+    match self {
+      Pattern::TestAll => return trigger == &Trigger::TestAll,
+      Pattern::TestFile { files } => {
+        if let Trigger::TestFile { file } = &trigger {
+          return files.matches(file);
+        }
       }
-    }
-    if let Pattern::TestFileLine { files } = &self {
-      if let Trigger::TestFileLine { file, line: _ } = &trigger {
-        return files.matches(file);
+      Pattern::TestFileLine { files } => {
+        if let Trigger::TestFileLine { file, line: _ } = &trigger {
+          return files.matches(file);
+        }
       }
     }
     false
@@ -35,12 +35,94 @@ mod tests {
     mod test_all {
       use crate::client;
       use crate::config;
+      use big_s::S;
 
       #[test]
       fn matches() {
         let config_trigger = config::Pattern::TestAll;
         let client_trigger = client::Trigger::TestAll;
         assert!(config_trigger.matches_trigger(&client_trigger));
+      }
+
+      #[test]
+      fn mismatch() {
+        let config_trigger = config::Pattern::TestAll;
+        let client_trigger = client::Trigger::TestFile { file: S("file") };
+        assert!(config_trigger.matches_trigger(&client_trigger));
+      }
+    }
+
+    mod test_file {
+      use crate::client;
+      use crate::config;
+      use big_s::S;
+
+      #[test]
+      fn matches() {
+        let config_trigger = config::Pattern::TestFile {
+          files: glob::Pattern::new("*.rs").unwrap(),
+        };
+        let client_trigger = client::Trigger::TestFile { file: S("foo.rs") };
+        assert!(config_trigger.matches_trigger(&client_trigger));
+      }
+
+      #[test]
+      fn mismatching_file() {
+        let config_trigger = config::Pattern::TestFile {
+          files: glob::Pattern::new("*.rs").unwrap(),
+        };
+        let client_trigger = client::Trigger::TestFile {
+          file: S("mismatch.go"),
+        };
+        assert!(!config_trigger.matches_trigger(&client_trigger));
+      }
+
+      #[test]
+      fn mismatching_type() {
+        let config_trigger = config::Pattern::TestFile {
+          files: glob::Pattern::new("*.rs").unwrap(),
+        };
+        let client_trigger = client::Trigger::TestAll;
+        assert!(!config_trigger.matches_trigger(&client_trigger));
+      }
+    }
+
+    mod test_file_line {
+      use crate::client;
+      use crate::config;
+      use big_s::S;
+
+      #[test]
+      fn matches() {
+        let config_trigger = config::Pattern::TestFileLine {
+          files: glob::Pattern::new("*.rs").unwrap(),
+        };
+        let client_trigger = client::Trigger::TestFileLine {
+          file: S("foo.rs"),
+          line: 3,
+        };
+        assert!(config_trigger.matches_trigger(&client_trigger));
+      }
+
+      #[test]
+      fn mismatching_file() {
+        let config_trigger = config::Pattern::TestFileLine {
+          files: glob::Pattern::new("*.rs").unwrap(),
+        };
+        let client_trigger = client::Trigger::TestFileLine {
+          file: S("mismatch.go"),
+          line: 3,
+        };
+        assert!(!config_trigger.matches_trigger(&client_trigger));
+      }
+
+      #[test]
+      fn mismatching_type() {
+        let config_trigger = config::Pattern::TestFileLine {
+          files: glob::Pattern::new("*.rs").unwrap(),
+        };
+        let client_trigger = client::Trigger::TestAll;
+        assert!(!config_trigger.matches_trigger(&client_trigger));
       }
     }
   }
