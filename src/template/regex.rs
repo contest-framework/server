d7765f254c12c87ba7d720ae7802099d794368af
Kevin Goslar <kevin.goslar@gmail.com>
2024-05-16 11:24:21 -0500

    regex.rs

--- src/template/regex.rs
+++ src/template/regex.rs
@@ -1,5 +1,6 @@
 use regex::Regex;
 
+/// provides a regex that matches a placeholder with the given text
 pub fn regex(placeholder: &str) -> regex::Regex {
   Regex::new(&format!("\\{{\\{{\\s*{}\\s*\\}}\\}}", placeholder)).unwrap()
 }
