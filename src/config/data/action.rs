5aced91befdecdf04067cbc21871a6c3220d820f
Kevin Goslar <kevin.goslar@gmail.com>
2024-05-15 21:09:15 -0500

    action.rs

--- src/config/data/action.rs
+++ src/config/data/action.rs
@@ -1,6 +1,6 @@
 use super::{Pattern, Var};
 
-/// Actions are executed when receiving a trigger.
+/// Actions are executed when receiving a command.
 #[derive(Debug, PartialEq)]
 pub struct Action {
   pub pattern: Pattern,
