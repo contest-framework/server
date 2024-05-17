510a4887ec888c7830c31bfa768cff0c9e89f4ef
Kevin Goslar <kevin.goslar@gmail.com>
2024-05-17 07:10:07 -0500

    ctrl_c.rs

--- src/cli/ctrl_c.rs
+++ src/cli/ctrl_c.rs
@@ -4,11 +4,10 @@ use crate::{channel, cli};
 
 /// captures Ctrl-C and messages it as a `Signal::Exit` message via the given sender
 pub fn handle(sender: channel::Sender) {
-  if let Err(err) = ctrlc::set_handler(move || {
-    if let Err(err) = sender.send(channel::Signal::Exit) {
-      cli::exit(&err.to_string());
-    }
-  }) {
-    cli::exit(&err.to_string());
-  }
+  ctrlc::set_handler(move || {
+    sender
+      .send(channel::Signal::Exit)
+      .unwrap_or_else(|err| cli::exit(&err.to_string()));
+  })
+  .unwrap_or_else(|err| cli::exit(&err.to_string()));
 }
