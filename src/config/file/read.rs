e155f2263aca74e91952a77f2fe7eef9d97b7075
Kevin Goslar <kevin.goslar@gmail.com>
2024-05-20 11:42:07 -0500

    Cargo.lock Cargo.toml extra_fields.feature fifo_data.rs read.rs

--- src/config/file/read.rs
+++ src/config/file/read.rs
@@ -1,11 +1,11 @@
 use super::super::Configuration;
 use super::{FileConfiguration, PATH};
 use crate::{Result, UserError};
-use std::fs::File;
+use std::fs;
 use std::io;
 
 pub fn read() -> Result<Configuration> {
-  let file = match File::open(PATH) {
+  let file_content = match fs::read_to_string(PATH) {
     Ok(file) => file,
     Err(err) if err.kind() == io::ErrorKind::NotFound => return Ok(Configuration::default()),
     Err(err) => {
@@ -15,7 +15,7 @@ pub fn read() -> Result<Configuration> {
     }
   };
   let file_data: FileConfiguration =
-    serde_json::from_reader(file).map_err(|err| UserError::ConfigFileInvalidContent {
+    json5::from_str(&file_content).map_err(|err| UserError::ConfigFileInvalidContent {
       err: err.to_string(),
     })?;
   file_data.into_domain()
