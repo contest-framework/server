819289afdd47a6074cc2085c93b2f9f1cf6f4f4f
Kevin Goslar <kevin.goslar@gmail.com>
2025-03-08 07:02:11 -0600

    Encapsulate clap into cli module

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -1,8 +1,10 @@
 //! everything around CLI input
 
+mod command;
 pub mod ctrl_c;
 mod exit;
 mod print_error;
 
+pub use command::Command;
 pub use exit::exit;
 pub use print_error::{error_color, print_error};
