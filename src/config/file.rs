e82d199e52f5ff9bd449090355ce88cf262fff43
Kevin Goslar <kevin.goslar@gmail.com>
2025-03-12 14:28:10 -0500

    Sort (#137)

--- src/config/file.rs
+++ src/config/file.rs
@@ -3,6 +3,13 @@
 use crate::config::VarSource;
 use serde::Deserialize;
 
+/// low-level, unvalidated `Configuration` data exactly how it is stored in the config file
+#[derive(Deserialize)]
+pub struct FileConfiguration {
+  pub actions: Vec<FileAction>,
+  pub options: Option<FileOptions>,
+}
+
 /// low-level, unvalidated `Action` data exactly how it is stored in the config file
 #[derive(Deserialize)]
 pub struct FileAction {
@@ -12,28 +19,12 @@ pub struct FileAction {
   pub vars: Option<Vec<FileVar>>,
 }
 
-/// low-level, unvalidated `AfterRun` data exactly how it is stored in the config file
-#[derive(Default, Deserialize, Eq, PartialEq)]
-#[serde(rename_all = "camelCase")]
-pub struct FileAfterRun {
-  pub newlines: Option<u8>,
-  pub indicator_lines: Option<u8>,
-  pub print_result: Option<bool>,
-}
-
-/// low-level, unvalidated `BeforeRun` data exactly how it is stored in the config file
-#[derive(Default, Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct FileBeforeRun {
-  pub clear_screen: Option<bool>,
-  pub newlines: Option<u8>,
-}
-
-/// low-level, unvalidated `Configuration` data exactly how it is stored in the config file
-#[derive(Deserialize)]
-pub struct FileConfiguration {
-  pub actions: Vec<FileAction>,
-  pub options: Option<FileOptions>,
+/// low-level, unvalidated `Var` data exactly how it is stored in the config file
+#[derive(Deserialize, Debug, Eq, PartialEq)]
+pub struct FileVar {
+  pub name: String,
+  pub source: VarSource,
+  pub filter: String,
 }
 
 /// low-level, unvalidated `Options` data exactly how it is stored in the config file
@@ -44,10 +35,19 @@ pub struct FileOptions {
   pub after_run: Option<FileAfterRun>,
 }
 
-/// low-level, unvalidated `Var` data exactly how it is stored in the config file
-#[derive(Deserialize, Debug, Eq, PartialEq)]
-pub struct FileVar {
-  pub name: String,
-  pub source: VarSource,
-  pub filter: String,
+/// low-level, unvalidated `BeforeRun` data exactly how it is stored in the config file
+#[derive(Default, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct FileBeforeRun {
+  pub clear_screen: Option<bool>,
+  pub newlines: Option<u8>,
+}
+
+/// low-level, unvalidated `AfterRun` data exactly how it is stored in the config file
+#[derive(Default, Deserialize, Eq, PartialEq)]
+#[serde(rename_all = "camelCase")]
+pub struct FileAfterRun {
+  pub newlines: Option<u8>,
+  pub indicator_lines: Option<u8>,
+  pub print_result: Option<bool>,
 }
