d8b53dd5a14df260a2cde536a878e5c8caf8381b
Kevin Goslar <kevin.goslar@gmail.com>
2025-03-12 20:01:58 -0500

    Initial JSON schema prototype (#146)

--- src/config/file.rs
+++ src/config/file.rs
@@ -1,54 +1,68 @@
 //! data structures as they are in the file
 
 use crate::config::VarSource;
+use schemars::JsonSchema;
 use serde::Deserialize;
 
-/// low-level, unvalidated `Configuration` data exactly how it is stored in the config file
-#[derive(Deserialize)]
+/// configuration data for <https://github.com/contest-framework/server>
+#[derive(Deserialize, JsonSchema)]
 pub struct FileConfiguration {
+  /// define the tests that Contest will run for you
   pub actions: Vec<FileAction>,
+  /// configure the layout and behavior
   pub options: Option<FileOptions>,
 }
 
-/// low-level, unvalidated `Action` data exactly how it is stored in the config file
-#[derive(Deserialize)]
+/// a particular test
+#[derive(Deserialize, JsonSchema)]
 pub struct FileAction {
   pub r#type: String,
+  /// the files for which this command applies as a glob expression
   pub files: Option<String>,
+  /// the command to run
   pub run: String,
+  /// define additional variables to use in the "run" string
   pub vars: Option<Vec<FileVar>>,
+  /// human-readable description of this action
   pub comment: Option<String>,
 }
 
-/// low-level, unvalidated `Var` data exactly how it is stored in the config file
-#[derive(Deserialize, Debug, Eq, PartialEq)]
+/// an additional variable that gets derived from the file content
+#[derive(Deserialize, Debug, Eq, JsonSchema, PartialEq)]
 pub struct FileVar {
+  /// name of the variable, available as "{{ name }}" later
   pub name: String,
+  /// the location in the file
   pub source: VarSource,
+  /// how the variable gets computed
   pub filter: String,
 }
 
-/// low-level, unvalidated `Options` data exactly how it is stored in the config file
-#[derive(Default, Deserialize)]
+#[derive(Default, Deserialize, JsonSchema)]
 #[serde(rename_all = "camelCase")]
 pub struct FileOptions {
+  /// configure behavior before Contest runs a test
   pub before_run: Option<FileBeforeRun>,
+  /// configure behavior after Contest runs a test
   pub after_run: Option<FileAfterRun>,
 }
 
-/// low-level, unvalidated `BeforeRun` data exactly how it is stored in the config file
-#[derive(Default, Deserialize)]
+#[derive(Default, Deserialize, JsonSchema)]
 #[serde(rename_all = "camelCase")]
 pub struct FileBeforeRun {
+  /// whether to clear the screen before a test run
   pub clear_screen: Option<bool>,
+  /// how many newlines to print before a test run
   pub newlines: Option<u8>,
 }
 
-/// low-level, unvalidated `AfterRun` data exactly how it is stored in the config file
-#[derive(Default, Deserialize, Eq, PartialEq)]
+#[derive(Default, Deserialize, Eq, JsonSchema, PartialEq)]
 #[serde(rename_all = "camelCase")]
 pub struct FileAfterRun {
+  /// how many newlines to print after a test run
   pub newlines: Option<u8>,
+  /// how many indicator lines (red or green) to print after a test run
   pub indicator_lines: Option<u8>,
+  /// whether to print "SUCCESS" or "FAILED" after a test run
   pub print_result: Option<bool>,
 }
---
+++ tests/json-schema.rs
@@ -0,0 +1,10 @@
+use contest::config::file::FileConfiguration;
+use schemars::schema_for;
+use std::fs;
+
+#[test]
+fn export_json_schema() {
+  let schema = schema_for!(FileConfiguration);
+  let text = serde_json::to_string_pretty(&schema).unwrap();
+  fs::write("documentation/schema.json", text).unwrap();
+}
