300a2223d8a3f30f73663ac3117cc8e707997e35
Kevin Goslar <kevin.goslar@gmail.com>
2025-11-15 06:36:22 -0600

    print_error.rs errors.rs lib.rs

--- src/errors.rs
+++ src/errors.rs
@@ -43,15 +43,15 @@ pub enum UserError {
 impl UserError {
   /// Provides human-readable messages for `UserError`.
   #[must_use]
-  pub fn messages(&self) -> (String, Option<String>) {
+  pub fn messages(self) -> (String, Option<String>) {
     match self {
       UserError::CannotCreateConfigFile { err } => (format!("cannot create configuration file: {err}"), None),
       UserError::CannotDetermineCurrentDirectory { err } => (format!("cannot determine the current directory: {err}"), None),
-      UserError::CannotReadFile { path, err } => (format!("cannot read file {path}"), Some(err.clone())),
-      UserError::CannotSplitShellString { source, err } => (format!("cannot split this shell string: {source}"), Some(err.clone())),
+      UserError::CannotReadFile { path, err } => (format!("cannot read file {path}"), Some(err)),
+      UserError::CannotSplitShellString { source, err } => (format!("cannot split this shell string: {source}"), Some(err)),
       UserError::ConfigFileInvalidContent { err } => (format!("Cannot parse configuration file: {err}"), None),
       UserError::ConfigFileError { err } => (format!("Cannot open configuration file: {err}"), None),
-      UserError::ConfigInvalidGlob { pattern, err } => (format!("Invalid glob pattern: {pattern}"), Some(err.clone())),
+      UserError::ConfigInvalidGlob { pattern, err } => (format!("Invalid glob pattern: {pattern}"), Some(err)),
       UserError::FifoAlreadyExists { path } => (
         format!("A fifo pipe \"{path}\" already exists."),
         Some(S(
@@ -69,8 +69,8 @@ impl UserError {
         )),
       ),
       UserError::FilesIsEmpty => (S(r#"The "files" field in your config file is empty"#), None),
-      UserError::InvalidRegex { regex, err } => (format!("invalid regex: {regex}"), Some(err.clone())),
-      UserError::InvalidTrigger { source: line, err } => (format!("cannot parse command received from client: {line}"), Some(err.clone())),
+      UserError::InvalidRegex { regex, err } => (format!("invalid regex: {regex}"), Some(err)),
+      UserError::InvalidTrigger { source: line, err } => (format!("cannot parse command received from client: {line}"), Some(err)),
       UserError::LineIsNotANumber { line } => (format!("the provided line ({line})is not a number"), None),
       UserError::LineNotAvailable => (
         S("Line not available"),
